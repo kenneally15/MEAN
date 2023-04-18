92911c359bb06ed0ebec38114056f6a1adb30d62
kenneally15 <kenneally15@yahoo.com>
2023-04-17 23:42:02 -0400

    Added back html variable

--- test.js
+++ test.js
@@ -7,6 +7,10 @@
 var path = require('path');
 var app = require(path.resolve('./config/lib/app'));
 
+//Testing commit change
+var html = require('html')
+
+
 app.init(function () {
   console.log('Initialized test automation');
 });
